8fb0acef2bbc2ed8c27161544c085645e81e50df
Doron Segal <doron2402@gmail.com>
2013-12-31 18:56:30 -0800

    reload using socket.io

---
+++ app.js
@@ -0,0 +1,51 @@
+var fs		= require('fs'),
+	url		= require('url'),
+	http	= require('http'),
+	path	= require('path'),
+	express	= require('express'),
+	app		= express(),
+	server  = http.createServer(app),
+	io		= require('socket.io').listen(server),
+	root	= __dirname,
+	port    = 8080,
+	watchers= {};
+
+
+app.use(function (req, res, next) {
+	
+	req.on('static', function(){
+		var file = url.parse(req.url).pathname,
+			mode = 'stylesheet';
+
+		if (file[file.length - 1] == '/'){
+			file += 'index.html';
+			mode = reload;
+		}
+
+		createWatcher(file, mode);
+	});
+	
+	next();
+
+});
+
+
+app.use(express.static(root));
+
+function createWatcher(file, event){
+	var absolute = path.join(root, file);
+
+	if (watchers[absolute])
+		return;
+
+	fs.watchFile(absolute, function(curr, prev){
+		if (curr.mtime !== prev.mtime){
+			io.sockets.emit(event, file);
+
+		}
+	});
+
+	watchers[absolute] = true;
+}
+
+server.listen(port);
